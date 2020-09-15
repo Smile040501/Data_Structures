eaaaf34dfe72170208787a433644f124f833a605
Mayank Singla <62458127+Smile040501@users.noreply.github.com>
2020-09-15 12:15:50 +0530

    Added implementation of Doubly Linked List

---
+++ Doubly_Linked_List/Node.h
@@ -0,0 +1,34 @@
+#ifndef _NODE_
+#define _NODE_
+
+class Node {			// Node class for representing a node of Doubly Linked List
+
+public:
+	// Attributes
+	Node *prev;
+	int data;
+	Node *next;
+	
+	// Constructors
+	Node();
+	Node(int data);
+	
+	// Destructor
+	~Node();
+};
+
+// Constructors
+Node::Node()
+: prev{nullptr}, data{}, next{nullptr} {
+}
+
+Node::Node(int data)
+: prev{nullptr}, data{data}, next{nullptr} {
+}
+
+// Destructor
+Node::~Node() {
+	delete next;
+}
+
+#endif // _NODE_
